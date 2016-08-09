f58c13a7acbe7c16c652499839238d8c9134778e
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2016-08-09 17:42:46 +0530

    Create 318B.cpp

---
+++ 318B.cpp
@@ -0,0 +1,95 @@
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//               佛祖保佑         永无BUG
+//
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <iomanip>
+#include <sstream>
+#include <bitset>
+#include <cstdlib>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <math.h>
+#include <ctime>
+#include <cstring>
+#include <climits>
+#include <list>
+using namespace std;
+
+#define sci(x) scanf("%d",&x)
+#define scli(x) scanf("%lld",&x)
+#define sc2i(x,y) scanf("%d%d",&x,&y)
+#define sc2li(x,y) scanf("%lld%lld",&x,&y)
+#define pb push_back
+#define X first
+#define Y second
+#define boost cin.tie(0);cout.tie(0)
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define pf printf
+#define MEM(a,val) memset(a,val,sizeof(a))
+#define pi 3.14159265359
+#define mp make_pair
+#define all(c) c.begin(),c.end() //eg sort(all(v));
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+int vis[4001][4001]={0};
+ll arr[4006]={0};
+int main(){
+  int n,m;
+  vector<pii> v;
+  cin >> n >> m;
+  int x,y;
+  for(int i=0;i<m;i++){
+     cin >> x >> y;
+     vis[x][y]=1;vis[y][x]=1;
+     v.pb(mp(x,y));
+     arr[x]++;arr[y]++;
+  }
+  ll ans=987654321;
+  for(int i=0;i<v.size();i++){
+     x=v[i].X;
+     y=v[i].Y;
+     for(int j=1;j<=n;j++){
+        if(j==x || j==y)continue;
+        else if(vis[x][j] && vis[y][j]){ans=min(ans,arr[x]+arr[y]+arr[j]-6);}
+     }
+  }
+ if(ans==987654321)cout << "-1\n";
+ else cout << ans << endl;
+ return 0;
+}
