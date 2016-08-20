943714d9694e3bf30cc5fc92527d22c414443b62
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2016-08-21 02:14:46 +0530

    Create 322B.cpp

---
+++ 322B.cpp
@@ -0,0 +1,85 @@
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//               佛祖保佑         永无BUG
+//
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <iomanip>
+#include <sstream>
+#include <bitset>
+#include <cstdlib>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <math.h>
+#include <ctime>
+#include <cstring>
+#include <climits>
+#include <list>
+using namespace std;
+
+#define sci(x) scanf("%d",&x)
+#define scli(x) scanf("%lld",&x)
+#define sc2i(x,y) scanf("%d%d",&x,&y)
+#define sc2li(x,y) scanf("%lld%lld",&x,&y)
+#define pb push_back
+#define X first
+#define Y second
+#define boost cin.tie(0);cout.tie(0)
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define pf printf
+#define MEM(a,val) memset(a,val,sizeof(a))
+#define pi 3.14159265359
+#define mp make_pair
+#define all(c) c.begin(),c.end() //eg sort(all(v));
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+
+int main(){
+  ll r,g,b;
+  ll x,y,z,ans=0;
+  cin >> r >> g >> b;
+  x=min(r,min(g,b));
+  ans+=x;
+  r-=x;g-=x;b-=x;
+  ans=ans+(r/3)+(g/3)+(b/3);
+  if((r%3==2 && g%3==2)||(r%3==2 && b%3==2)||(g%3==2 && b%3==2)){
+   if(x>=1)
+   ans++;
+ }
+  cout << ans << endl;
+  return 0;
+}
