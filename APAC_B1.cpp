64b309315f9847bb2128cd2bad8494ebb20be232
Nilotpal Mrinal <nilotpalmrinal9797@gmail.com>
2016-08-28 18:06:08 +0530

    Create APAC_B1.cpp

---
+++ APAC_B1.cpp
@@ -0,0 +1,84 @@
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//               佛祖保佑         永无BUG
+//
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <iomanip>
+#include <sstream>
+#include <bitset>
+#include <cstdlib>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <cstdio>
+#include <cctype>
+#include <cmath>
+#include <math.h>
+#include <ctime>
+#include <cstring>
+#include <climits>
+#include <list>
+using namespace std;
+
+#define sci(x) scanf("%d",&x)
+#define scli(x) scanf("%lld",&x)
+#define sc2i(x,y) scanf("%d%d",&x,&y)
+#define sc2li(x,y) scanf("%lld%lld",&x,&y)
+#define pb push_back
+#define X first
+#define Y second
+#define boost cin.tie(0);cout.tie(0)
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define pf printf
+#define MEM(a,val) memset(a,val,sizeof(a))
+#define pi 3.14159265359
+#define mp make_pair
+#define all(c) c.begin(),c.end() //eg sort(all(v));
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+
+int main(){
+ int t;
+ int xy=0;
+ cin >> t;
+ while(t--){
+  xy++;
+  ll l,r;
+  cin >> l >> r;
+  ll x=min(l,r);
+  ll ans=(x*(x+1))/2;
+  printf("Case #%d: %lld\n",xy,ans);
+ }
+  return 0;
+}
